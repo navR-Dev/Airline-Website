545c9f28542bd4cbe3d217c7cb9bc63a58ad4baf
naveenRDev <114591918+naveenRDev@users.noreply.github.com>
2023-11-01 11:35:16 +0530

    Added backend code

---
+++ backend-code/server.js
@@ -0,0 +1,57 @@
+var express = require("express"),
+  bodyParser = require("body-parser");
+const swaggerJsdoc = require("swagger-jsdoc"),
+  swaggerUi = require("swagger-ui-express");
+const cors = require("cors");
+
+const app = express();
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use("/airline", require("./routes/airline"));
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Airline website API",
+      version: "0.1.0",
+      description:
+        "This is the API designed to manage all the backend operations for the Airline website",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:5000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs, {
+    explorer: true,
+    customCssUrl:
+      "https://cdn.jsdelivr.net/npm/swagger-ui-themes@3.0.0/themes/3.x/theme-newspaper.css",
+  })
+);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
+
+console.debug("Server listening on port: " + PORT);
